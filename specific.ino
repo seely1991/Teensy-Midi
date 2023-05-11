c9e1cdcaca34ce8b4f14e7320dcce04cb63c89a2
seely1991 <40604521+seely1991@users.noreply.github.com>
2023-05-11 13:25:22 -0600

    Update specific.ino
    used millis() to set clock for note timeout instead of delay for faster execution.

--- specific.ino
+++ specific.ino
@@ -13,6 +13,7 @@ Bounce buttons[3] = {
 int MIDI_NOTE_VALS[5] = {60, 61, 62, 63, 64};
 
 bool button_state[5];
+int note_end_time[5];
   
 int duration = 200;
 
@@ -26,35 +27,37 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < 3; i++){
+  for (int i = 0; i < 5; i++){
+    if (note_end_time[i] && millis() > note_end_time[i]){
+      usbMIDI.sendNoteOff(MIDI_NOTE_VALS[i],0,MIDI_CHAN);
+      button_state[i] = 0;
+      note_end_time[i] = 0;
+    };
+    if (i < 3){
       buttons[i].update();
-    }
+    };
+  };
   
   for (int i = 0; i< 3; i++){
     if (buttons[i].fallingEdge()){
       if (button_state[0] && button_state[1]){
         button_state[0], button_state[1] = 0;
         button_state[3] = 1;
+        note_end_time[3] = millis() + duration;
         usbMIDI.sendNoteOn(MIDI_NOTE_VALS[3],127,MIDI_CHAN);
-        delay(duration);
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[3],0,MIDI_CHAN);
-        button_state[3] = 0;
       }else if (button_state[1] && button_state[2]){
         button_state[1], button_state[2] = 0;
         button_state[4] = 1;
+        note_end_time[4] = millis() + duration;
         usbMIDI.sendNoteOn(MIDI_NOTE_VALS[4],127,MIDI_CHAN);
-        delay(duration);
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[4],0,MIDI_CHAN);
-        button_state[4] = 0;
       }else{ 
         // normal button press
         button_state[i] = 1;
       };
     }else if (buttons[i].risingEdge() && button_state[i]){
       button_state[i] = 0;
-      usbMIDI.sendNoteOn(MIDI_NOT_VALS[i],127,MIDI_CHAN);
-      delay(duration);
-      usbMIDI.sendNoteOff(MIDI_NOTE_VALS[i], 0, MIDI_CHAN);
+      usbMIDI.sendNoteOn(MIDI_NOTE_VALS[i],127,MIDI_CHAN);
+      note_end_time[i] = millis() + duration;
     };
     delay(25);
   };
