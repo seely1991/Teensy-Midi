7f479bc207e5348673f592e5155e58cd6f931651
seely1991 <40604521+seely1991@users.noreply.github.com>
2023-05-11 09:25:50 -0600

    momentary press
    buttons send midi when button is released (not pressed)
    
    multiple buttons together send additional midi signal
    
    all are set by delay function, may want to update to scroll up and down more quickly

--- specific.ino
+++ specific.ino
@@ -14,6 +14,7 @@ int MIDI_NOTE_VALS[5] = {60, 61, 62, 63, 64};
 
 bool button_state[5];
   
+int duration = 200;
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,31 +33,28 @@ void loop() {
   for (int i = 0; i< 3; i++){
     if (buttons[i].fallingEdge()){
       if (button_state[0] && button_state[1]){
+        button_state[0], button_state[1] = 0;
         button_state[3] = 1;
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[0], 0, MIDI_CHAN);
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[1], 0, MIDI_CHAN);
-        usbMIDI.sendNoteOn(MIDI_NOT_VALS[3],127,MIDI_CHAN);
+        usbMIDI.sendNoteOn(MIDI_NOTE_VALS[3],127,MIDI_CHAN);
+        delay(duration);
+        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[3],0,MIDI_CHAN);
+        button_state[3] = 0;
       }else if (button_state[1] && button_state[2]){
+        button_state[1], button_state[2] = 0;
         button_state[4] = 1;
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[1], 0, MIDI_CHAN);
-        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[2], 0, MIDI_CHAN);
-        usbMIDI.sendNoteOn(MIDI_NOT_VALS[4],127,MIDI_CHAN);
+        usbMIDI.sendNoteOn(MIDI_NOTE_VALS[4],127,MIDI_CHAN);
+        delay(duration);
+        usbMIDI.sendNoteOff(MIDI_NOTE_VALS[4],0,MIDI_CHAN);
+        button_state[4] = 0;
       }else{ 
         // normal button press
         button_state[i] = 1;
-        usbMIDI.sendNoteOn(MIDI_NOTE_VALS[i], 127, MIDI_CHAN);
       };
-    }else if (buttons[i].risingEdge()){
+    }else if (buttons[i].risingEdge() && button_state[i]){
       button_state[i] = 0;
+      usbMIDI.sendNoteOn(MIDI_NOT_VALS[i],127,MIDI_CHAN);
+      delay(duration);
       usbMIDI.sendNoteOff(MIDI_NOTE_VALS[i], 0, MIDI_CHAN);
-      if (i < 2 && button_state[3]){
-        button_state[3] = 0;
-        usbMIDI.sendNoteOff(MIDI_NOT_VALS[3],0,MIDI_CHAN);
-      };
-      if (i > 0 && button_state[4]){
-        button_state[4] = 0;
-        usbMIDI.sendNoteOff(MIDI_NOT_VALS[4],0,MIDI_CHAN);
-      };
     };
     delay(25);
   };
